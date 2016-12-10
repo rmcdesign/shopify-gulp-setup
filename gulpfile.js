0c55e63182a02c3fef720208e8d8f489f6e1d766
rmcdesign <info@rmc-design.co.uk>
2016-12-10 14:20:39 +0000

    Code clean up

--- gulpfile.js
+++ gulpfile.js
@@ -19,86 +19,86 @@ var gulp = require( 'gulp' ),
     autoprefixer = require('autoprefixer-core'),
     cssnano = require('gulp-cssnano'),
     ///////
-    browserify = require('browserify')
-    watchify = require('watchify')
+    browserify = require('browserify'),
+    watchify = require('watchify'),
     source = require('vinyl-source-stream'),
     // Optimizes images
-    imagemin   = require('gulp-imagemin')
+    imagemin   = require('gulp-imagemin'),
     changed    = require('gulp-changed');
 
 function handleErrors() {
-  var args = Array.prototype.slice.call(arguments);
+    var args = Array.prototype.slice.call(arguments);
 
-  // Send error to notification center with gulp-notify
-  notify.onError({
-    title: "Compile Error",
-    message: "<%= error %>"
-  }).apply(this, args);
+    // Send error to notification center with gulp-notify
+    notify.onError({
+        title: "Compile Error",
+        message: "<%= error %>"
+    }).apply(this, args);
 
-  // Keep gulp from hanging on this task
-  this.emit('end');
+    // Keep gulp from hanging on this task
+    this.emit('end');
 }
 
 gulp.task('shopifywatch', function() {
-  var options = {
-    "basePath": "./Timber/"
-  };
+    var options = {
+        "basePath": "./Timber/"
+    };
 
-  return watch('./Timber/+(assets|layout|config|snippets|templates|locales)/**')
-  .pipe(gulpShopify(config.shopify_api_key, config.shopify_api_password, config.shopify_url, null, options));
+    return watch('./Timber/+(assets|layout|config|snippets|templates|locales)/**')
+        .pipe(gulpShopify(config.shopify_api_key, config.shopify_api_password, config.shopify_url, null, options));
 });
 
 gulp.task('sass', function () {
-  gulp.src(scss_source)
-    .pipe( sass() )
-    .on('error', handleErrors)
-    .pipe( gcmq() )
-    .pipe( postcss([ autoprefixer({ browsers: ['last 5 versions'] }) ]) )
-    .pipe( gulp.dest( css_dest ) )
-    .pipe( sourcemaps.init())
-    .pipe( cssnano({
-      safe: true,
-      discardComments: {removeAll: true}
-    }) )
-    .pipe( rename( { suffix: minify_suffix } ) )
-    .pipe( sourcemaps.write('.') )
-    .pipe( gulp.dest( css_dest ) );
+    gulp.src(scss_source)
+        .pipe( sass() )
+        .on('error', handleErrors)
+        .pipe( gcmq() )
+        .pipe( postcss([ autoprefixer({ browsers: ['last 5 versions'] }) ]) )
+        .pipe( gulp.dest( css_dest ) )
+        .pipe( sourcemaps.init())
+        .pipe( cssnano({
+            safe: true,
+            discardComments: {removeAll: true}
+        }) )
+        .pipe( rename( { suffix: minify_suffix } ) )
+        .pipe( sourcemaps.write('.') )
+        .pipe( gulp.dest( css_dest ) );
 });
 
 gulp.task('browserify', function() {
-  return browserify('./lib/js/app.js')
-      .bundle()
-      .on('error', handleErrors)
-      //Pass desired output filename to vinyl-source-stream
-      .pipe(source('bundle.js'))
-      // Start piping stream to tasks!
-      .pipe(gulp.dest('./Timber/assets/'));
+    return browserify('./lib/js/app.js')
+        .bundle()
+        .on('error', handleErrors)
+        //Pass desired output filename to vinyl-source-stream
+        .pipe(source('bundle.js'))
+        // Start piping stream to tasks!
+        .pipe(gulp.dest('./Timber/assets/'));
 });
 
 gulp.task('images', function() {
-  return gulp.src('./lib/images/**')
-    .pipe(changed('./Timber/assets/')) // Ignore unchanged files
-    .pipe(imagemin()) // Optimize
-    .pipe(gulp.dest('./Timber/assets/'))
+    return gulp.src('./lib/images/**')
+        .pipe(changed('./Timber/assets/')) // Ignore unchanged files
+        .pipe(imagemin()) // Optimize
+        .pipe(gulp.dest('./Timber/assets/'))
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./lib/scss/**/*.scss', ['sass']);
-  gulp.watch('./lib/js/**/*.js', ['browserify']);
-  gulp.watch('lib/images/*.{jpg,jpeg,png,gif,svg}', ['images']);
+    gulp.watch('./lib/scss/**/*.scss', ['sass']);
+    gulp.watch('./lib/js/**/*.js', ['browserify']);
+    gulp.watch('lib/images/*.{jpg,jpeg,png,gif,svg}', ['images']);
 
-  var watcher = watchify(browserify({
-    // Specify the entry point of your app
-    entries: ['./lib/js/app.js'],
-    debug: true,
-    cache: {}, packageCache: {}, fullPaths: true
-  }));
+    var watcher = watchify(browserify({
+        // Specify the entry point of your app
+        entries: ['./lib/js/app.js'],
+        debug: true,
+        cache: {}, packageCache: {}, fullPaths: true
+    }));
 
-  return watcher.on('update', function() {
-    watcher.bundle()
-      .pipe(source('bundle.js'))
-      .pipe(gulp.dest('./Timber/assets/'))
-  })
+    return watcher.on('update', function() {
+        watcher.bundle()
+            .pipe(source('bundle.js'))
+            .pipe(gulp.dest('./Timber/assets/'))
+    })
 });
 
 // Default gulp action when gulp is run
